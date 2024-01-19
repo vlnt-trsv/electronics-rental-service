b5f05d684ef161b58d44c860d116ef62be9f5505
Valentin <111338107+vlnt-trsv@users.noreply.github.com>
2024-01-19 13:47:36 +0700

    some changes

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-import App from "./components/app/App.jsx";
-import "./main.scss";
+import App from "./components/app/App.tsx";
+import "./main.css";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <BrowserRouter>
       <App />
