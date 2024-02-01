ef1e37b64cd3cff047dcfd781e3ba24f3d47118c
Valentin <111338107+vlnt-trsv@users.noreply.github.com>
2024-02-02 01:00:05 +0700

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,15 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./components/app/App.tsx";
 import "./main.css";
+import { Provider } from "react-redux";
+import { store } from "./redux/store";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   // <React.StrictMode>
-    <BrowserRouter>
+  <BrowserRouter>
+    <Provider store={store}>
       <App />
-    </BrowserRouter>
+    </Provider>
+  </BrowserRouter>
   // </React.StrictMode>
 );
