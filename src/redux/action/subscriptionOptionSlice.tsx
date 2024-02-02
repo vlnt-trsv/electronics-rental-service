f97b13583878d165aa122549be4b98b4a2f36512
Valentin <111338107+vlnt-trsv@users.noreply.github.com>
2024-02-02 21:11:03 +0700

    fix redux

--- src/redux/action/subscriptionOptionSlice.tsx
+++ src/redux/action/subscriptionOptionSlice.tsx
@@ -4,10 +4,10 @@ import { createSlice } from "@reduxjs/toolkit";
 const initialState = {
   selectedSubscription: null,
   subscriptionOptions: [
-    { id: 1, duration: "14 дней", price: "3 999 ₽" },
-    { id: 2, duration: "1 месяц", price: "3 999 ₽/мес" },
-    { id: 3, duration: "2 месяца", price: "3 999 ₽/мес" },
-    { id: 4, duration: "3 месяца", price: "3 999 ₽/мес" },
+    { id: 1, duration: "14 дней", price: 990 },
+    { id: 2, duration: "1 месяц", price: 1899 },
+    { id: 3, duration: "2 месяца", price: 2699 },
+    { id: 4, duration: "3 месяца", price: 3499 },
   ],
 };
 
