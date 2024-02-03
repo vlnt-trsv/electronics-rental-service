3c96f9ca774aa31239885859f56b587dead1aa9a
Valentin <111338107+vlnt-trsv@users.noreply.github.com>
2024-02-03 21:32:39 +0700

    update `main.tsx`

--- src/main.tsx
+++ src/main.tsx
@@ -4,12 +4,15 @@ import App from "./components/app/App.tsx";
 import "./main.css";
 import { Provider } from "react-redux";
 import { store } from "./redux/store";
+import ApiProvider from "./context/ApiProvider.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   // <React.StrictMode>
   <BrowserRouter>
     <Provider store={store}>
-      <App />
+      <ApiProvider>
+        <App />
+      </ApiProvider>
     </Provider>
   </BrowserRouter>
   // </React.StrictMode>
