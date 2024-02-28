7ece18c3c43d6ba7c2e1be9f38d838187040b48f
Valentin <111338107+vlnt-trsv@users.noreply.github.com>
2024-02-29 03:24:21 +0700

    update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,9 @@ import react from "@vitejs/plugin-react-swc"
 import { defineConfig } from "vite"
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
