7271f2ef6e048e67062a112111f9fcab20c48fac
Valentin <111338107+vlnt-trsv@users.noreply.github.com>
2024-03-01 22:20:15 +0700

    update layout page

--- src/pages/Layout.tsx
+++ src/pages/Layout.tsx
@@ -1,15 +1,13 @@
-// import { Outlet } from "react-router-dom";
-
 import Footer from "@/components/mainPage/footer/Footer.tsx";
 import Header from "@/components/mainPage/header/Header.tsx";
 import MiddleSection from "@/components/mainPage/middleSection/MiddleSection.tsx";
 import UpperSection from "@/components/mainPage/upperSection/UpperSection.tsx";
-
-
+import Background from "@/components/ui/anim/background";
 
 const Layout = () => {
   return (
     <>
+      <Background />
       <Header />
       <UpperSection />
       <MiddleSection />
