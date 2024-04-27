b9302f92a122edf938c30444eabbce5094dc3357
Valentin <111338107+vlnt-trsv@users.noreply.github.com>
2024-04-27 15:33:01 +0700

    update `main.tsx`
    Удалил провайдер для redux

--- src/main.tsx
+++ src/main.tsx
@@ -1,19 +1,15 @@
 import ReactDOM from "react-dom/client";
+import { Suspense } from "react";
 import { BrowserRouter } from "react-router-dom";
-import App from "./components/app/App.tsx";
-import "./main.css";
-import { Provider } from "react-redux";
-import { PersistGate } from "redux-persist/integration/react";
-import { store, persistor } from "./redux/store";
+import App from "./app/App.tsx";
+import "@/app/App.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  // <React.StrictMode> 
-  <Provider store={store}>
-    <PersistGate loading={"Loading..."} persistor={persistor}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </PersistGate>
-  </Provider>
+  // <React.StrictMode>
+  <BrowserRouter>
+    <Suspense fallback="Loading...">
+      <App />
+    </Suspense>
+  </BrowserRouter>
   // </React.StrictMode>
 );
