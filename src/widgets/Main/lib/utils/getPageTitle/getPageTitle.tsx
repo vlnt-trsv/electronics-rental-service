8a8128b4a828b7aadfe18492986bb5f21ed27b7b
Valentin <111338107+vlnt-trsv@users.noreply.github.com>
2024-05-03 18:59:53 +0700

    update widgets: main component

--- src/widgets/Main/lib/utils/getPageTitle/getPageTitle.tsx
+++ src/widgets/Main/lib/utils/getPageTitle/getPageTitle.tsx
@@ -1,34 +1,27 @@
-// getPageTitle.js
-import pageTitles from './pageTitles';
+import pageTitles from "./pageTitles";
 
-const getPageTitle = (pathname, params) => {
-  // Разбиваем путь на сегменты
-  const pathSegments = pathname.split('/').filter(Boolean);
+const getPageTitle = (pathname: string) => {
+  const pathSegments = pathname.split("/").filter(Boolean);
 
   // Проходим по каждому шаблону в pageTitles
   for (let pathTemplate in pageTitles) {
-    // Разбиваем шаблон на сегменты
-    const templateSegments = pathTemplate.split('/').filter(Boolean);
+    const templateSegments = pathTemplate.split("/").filter(Boolean);
 
-    // Проверяем, соответствует ли путь шаблону
     if (pathSegments.length === templateSegments.length) {
       let isMatch = true;
       const dynamicValues = {};
 
       for (let i = 0; i < templateSegments.length; i++) {
-        if (templateSegments[i].startsWith(':')) {
-          // Это динамический сегмент
+        if (templateSegments[i].startsWith(":")) {
           const paramName = templateSegments[i].slice(1);
           dynamicValues[paramName] = pathSegments[i];
         } else if (templateSegments[i] !== pathSegments[i]) {
-          // Несоответствие статического сегмента
           isMatch = false;
           break;
         }
       }
 
       if (isMatch) {
-        // Формируем заголовок, подставляя динамические значения
         let title = pageTitles[pathTemplate];
         for (let key in dynamicValues) {
           title = title.replace(`:${key}`, dynamicValues[key]);
@@ -38,8 +31,7 @@ const getPageTitle = (pathname, params) => {
     }
   }
 
-  // Возвращаем заголовок по умолчанию, если не найдено соответствие
-  return "Null";
+  return "Undefined";
 };
 
 export default getPageTitle;
--- src/widgets/Main/ui/Main.tsx
+++ src/widgets/Main/ui/Main.tsx
@@ -1,22 +1,20 @@
-import { Outlet, useLocation, useParams } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 import styles from "./Main.module.scss";
 import { Greeting } from "@/features";
 import getPageTitle from "../lib/utils/getPageTitle/getPageTitle";
 
 export default function Main() {
   const location = useLocation();
-  const params = useParams();
   const showProfileInfo = location.pathname.startsWith("/accountPage/");
-
-  console.log("@", location.pathname)
-  console.log("@", params)
 
   return (
     <div className={styles.main}>
-      <div className={styles.main__title}>
-        {getPageTitle(location.pathname, params)}
+      <div className={styles.main__container}>
+        <div className={styles.main__title}>
+          {getPageTitle(location.pathname)}
+        </div>
+        {/* <div className={styles.main__sblock}>Filter Section - INDEV</div> */}
       </div>
-      <hr />
       <div className={styles.main__wrapper}>
         {showProfileInfo ? <Outlet /> : <Greeting />}
       </div>
