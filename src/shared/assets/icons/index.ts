ad4ef5b1010f9a4f9090b46fb06d7cdbe5d50ef4
Valentin Tarasov <111338107+vlnt-trsv@users.noreply.github.com>
2024-06-15 22:28:19 +0700

    update shared

---
+++ src/shared/assets/icons/index.ts
@@ -0,0 +1,41 @@
+import {
+  IoExitOutline as ExitIcon,
+  IoNotifications as NotificationIcon,
+} from "react-icons/io5";
+import {
+  RiSettings3Fill as SettingIcon,
+  RiAccountCircleFill as AccountIcon,
+} from "react-icons/ri";
+import { BsArrowDownLeftSquareFill as ArrowDownLeftIcon } from "react-icons/bs";
+import {
+  FaTelegramPlane as TelegramIcon,
+  FaHamburger as BurgerOpen,
+} from "react-icons/fa";
+import {
+  FaTelegram as TelegramIconCirle,
+  FaUser as User,
+} from "react-icons/fa";
+import { MdEmail as EmailIcon } from "react-icons/md";
+import { FaCircleCheck as CheckIcon } from "react-icons/fa6";
+import {
+  IoLogoWhatsapp as WhatsappIcon,
+  IoMdClose as BurgerClose,
+} from "react-icons/io";
+import { AiFillInstagram as InstagramIcon } from "react-icons/ai";
+
+export {
+  ExitIcon,
+  SettingIcon,
+  AccountIcon,
+  ArrowDownLeftIcon,
+  TelegramIcon,
+  EmailIcon,
+  CheckIcon,
+  User,
+  TelegramIconCirle,
+  WhatsappIcon,
+  InstagramIcon,
+  NotificationIcon,
+  BurgerOpen,
+  BurgerClose,
+};
